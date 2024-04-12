36c94b27d7b30d61f81b5d7b93db0e5b55f47e25
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-04-12 14:56:53 +0100

    BoredomBuster API played with, doesn't work well as too limited. JSON built with potential fallbacks.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero.jsx";
+import MainFeature from "./components/MainFeature.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <Hero />
+    <MainFeature />
     <App />
   </React.StrictMode>
 );
